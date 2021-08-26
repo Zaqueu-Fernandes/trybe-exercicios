a89bd16bd8e379437d1d16d5edaa59fa7013d769
Zaqueu <zaqueufernandes@gmail.com>
2021-08-26 16:28:45 -0300

    Adiciona dois arquivos exercicios Array e loop For P1 & P2.js

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Array e loop For P1.js
@@ -0,0 +1,12 @@
+
+let pizzas = [' 4 quejos', ' Frango com Catupiry', ' Maguerita', ' Palmito', ' Chocolate'];
+pizzas[5] = ' Peito de Peru'; // Adiciona o novo sabor a posição 5 do Array
+pizzas.push(' Peito de Avestruz'); // Adiciona o novo sabor na ultima posição do Array
+
+console.log(pizzas); // Exibe todos os itens do Array
+console.log(pizzas[0]); // Exibe o Item do Array que tá na primeira posição
+console.log(pizzas.length); // Exibe o comprimento do Array, ou seja, a quantidades de itens do Array
+console.log(pizzas.sort()); // Exibe todos os itens do Array em ordem alfabetica
+
+//VÍDEO EXPLICATIVO =>  https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-array-e-loop-for/3d453622-6c99-46af-a884-fecc68811230/conteudos/137d5d54-c91a-48c8-bd64-d1a0551608c0/arrays-listas/36ef8665-27b5-42d7-965f-6aedc9f709f1?use_case=next_button
+
