ef4d1b357e5754f210da6cee71545af9ae18f36e
Zaqueu <zaqueufernandes@gmail.com>
2021-08-30 19:32:51 -0300

    Corrigido os exercicios praticando - Objeto ex1 & praticando - Objeto ex2

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticando - Objeto ex1.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/praticando - Objeto ex1.js
@@ -4,9 +4,9 @@ let player = {
     name:'Marta',
     latsName:'Silva',
     age: 34,
-    medals = {golden: 2, silver: 3},
+    medals: {golden: 2, silver: 3}
 };     
   
 //2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: 
 // "A jogadora Marta Silva tem 34 anos de idade".
-console.log('a jogadora ' + player.name + player.latsName + ' ' +' tem ' player.age + ' de idade');
+console.log('a jogadora ' + player.name + player.latsName + ' ' +' tem ' + player.age + ' de idade');
