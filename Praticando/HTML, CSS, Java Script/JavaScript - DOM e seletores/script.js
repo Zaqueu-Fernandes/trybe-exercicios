b9cdafcb7cd1bd0ef9d5d1ee0ff04db392ae8b11
Zaqueu <zaqueufernandes@gmail.com>
2021-09-03 17:59:29 -0300

    Criaçao da estrutura HTML e coneccao ao Script JS

---
+++ Praticando/HTML, CSS, Java Script/JavaScript - DOM e seletores/script.js
@@ -0,0 +1,13 @@
+// document.getElementById("page-title"); //acessa o elemento dentro de um ID "page-title"
+
+// console.log(document.getElementById("page-title")); //retorna toda estrutura do ID "page-title"
+
+// console.log(document.getElementById("page-title").innerHTML); //retorna todo o texto dentro do intervalo da ID "page-title" , inclusive as tags
+
+// console.log(document.getElementById("page-title").innerText); //retorna apenas o texto dentro do intervalo da ID "page-title" , sem considerar as tags
+
+document.getElementById("page-title").innerText = "Avatar - Meu Filme Favorito";
+
+document.getElementById("second-paragraph").innerText = "O Filme AVATAR me fez refletir no quanto nós seres humanos destruimos a natureza sem pensar nas consequências";
+
+document.getElementById("subtitle").innerText = "Filme fascinante e reflexivo com incríveis efeitos especiais muito realisticos";
